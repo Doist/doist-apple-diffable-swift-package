bc2016792c4a47665fac61621a0267c26633488c
Jaime Azevedo <jaime@doist.com>
2021-03-19 13:56:40 +0000

    Adds DiffableSectionInitializer.

--- Sources/Diffable/DiffableSection.swift
+++ Sources/Diffable/DiffableSection.swift
@@ -8,4 +8,9 @@
 public struct DiffableSection<SectionID: Diffable, RowID: Diffable> {
     public let identifier: SectionID
     public let rows: [RowID]
+
+    public init(identifier: SectionID, rows: [RowID]) {
+        self.identifier = identifier
+        self.rows = rows
+    }
 }
