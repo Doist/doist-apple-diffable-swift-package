b7b5e944e758529d6f121cd496a6402658972359
Jaime Azevedo <jaime@doist.com>
2021-05-27 17:15:28 +0100

    Improved Replace funcions.

--- Sources/DiffableDataSource/DiffableTableViewDataSource+Replace.swift
+++ Sources/DiffableDataSource/DiffableTableViewDataSource+Replace.swift
@@ -11,30 +11,27 @@ extension DiffableTableViewDataSource {
     // MARK: - Public API
 
     public func replace(with sections: [DiffableSection<SectionID, RowID>], animatingDifferences: Bool = true) {
-        checkUniqueDiffableSections(sections: sections) { result in
-            switch result {
-            case let .success(sections):
-                self.unsafeReplace(sections: sections, animatingDifferences: animatingDifferences)
+        switch checkUniqueDiffableSections(sections: sections) {
+        case let .success(sections):
+            self.unsafeReplace(sections: sections, animatingDifferences: animatingDifferences)
 
-            case let .failure(error):
-                logFault(error: error)
-            }
+        case let .failure(error):
+            logFault(error: error)
         }
     }
 
     public func checkUniqueDiffableSections(
-        sections: [DiffableSection<SectionID, RowID>],
-        result: (Result<[DiffableSection<SectionID, RowID>], DiffableError>) -> Void
-    ) {
+        sections: [DiffableSection<SectionID, RowID>]
+    ) -> Result<[DiffableSection<SectionID, RowID>], DiffableError> {
         let sectionIDs = sections.map { $0.identifier }
         let rowIDs = sections.flatMap { $0.rows }
 
         if sectionIDs.unique().count != sectionIDs.count {
-            result(.failure(DiffableError.sectionsNotUnique))
+            return .failure(DiffableError.sectionsNotUnique)
         } else if rowIDs.unique().count != rowIDs.count {
-            result(.failure(DiffableError.rowsNotUnique))
+            return .failure(DiffableError.rowsNotUnique)
         } else {
-            result(.success(sections))
+            return .success(sections)
         }
     }
 
